b903d8f5efd357e86be6138db4e58a3301231aed
Oscar Cuenca <ocuencamoreno@gmail.com>
2022-01-23 22:28:32 -0500

    Ready Code

---
+++ generator/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import random 
+# from django.http import HttpResponse
+
+def about(request):
+    return render(request, 'about.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def password(request):
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ''
+
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('-_+!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+        
+    for _ in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'password.html', {'password': generated_password})
